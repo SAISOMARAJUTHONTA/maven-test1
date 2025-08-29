be8a94c266ae5329443ad4b9d661f8abc7f25623
sai somaraju <somarajusai04@gmail.com>
2025-08-29 10:28:46 +0530

    SOMARAJUT0001 - Wrote the code for checking odd or even

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,13 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    public void isOddOrEven(int number) {
+        System.out.println("I am Somaraju");
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
